7b2135b75856f3ca5346c8dfe3e2552f6568157d
Kishan Patel <mail@kishanpatel.dev>
2023-04-05 07:28:47 -0400

    Add emr creation and sensor

--- airflow/dags/02_process_with_spark_dag.py
+++ airflow/dags/02_process_with_spark_dag.py
@@ -6,6 +6,8 @@ from airflow.providers.amazon.aws.operators.emr import (
     EmrCreateJobFlowOperator,
     EmrModifyClusterOperator,
     EmrTerminateJobFlowOperator,
+    EmrStepSensor,
+    EmrJobFlowSensor
 )
 import os
 from datetime import datetime
@@ -43,10 +45,68 @@ with DAG(
         },
     )
 
-    upload_script_task
+    SPARK_STEPS = [
+    {
+        "Name": "run1",
+        "ActionOnFailure": "CANCEL_AND_WAIT",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": ["spark-submit", 
+                     "--deploy-mode", 
+                     "cluster",
+                     "s3://weather-data-kpde/code/02_process_s3_parquet.py"],
+        },
+    }
+    ]
 
-# create cluster
+    JOB_FLOW_OVERRIDES = {
+    "Name": "emr-cluster-kp",
+    "ReleaseLabel": "emr-6.9.0",
+    "Applications": [{"Name": "Spark"}],
+    "Instances": {
+        "InstanceGroups": [
+            {
+                "Name": "Primary node",
+                "Market": "ON_DEMAND",
+                "InstanceRole": "MASTER",
+                "InstanceType": "m5.xlarge",
+                "InstanceCount": 1,
+            },
+        ],
+        "KeepJobFlowAliveWhenNoSteps": False,
+        "TerminationProtected": False,
+    },
+    "Steps": SPARK_STEPS,
+    "JobFlowRole": "emr-ec2-role",
+    "ServiceRole": "emr-role",
+    }
 
-# run job
+    # create cluster and run job
+    create_emr_cluster_task = EmrCreateJobFlowOperator(
+        task_id="create_emr_cluster_task",
+        job_flow_overrides=JOB_FLOW_OVERRIDES,
+        # aws_conn_id="aws_default",
+        # emr_conn_id="emr_default",
+    )
 
-# terminate cluster
+    # last_step = len(SPARK_STEPS) - 1 # this value will let the sensor know the last step to watch
+    # check_step_task = EmrStepSensor(
+    #     task_id="check_step_task",
+    #     job_flow_id="{{ task_instance.xcom_pull('create_emr_cluster_task', key='return_value') }}",
+    #     step_id="{{ task_instance.xcom_pull(task_ids='add_steps', key='return_value')["
+    #     + str(last_step)
+    #     + "] }}",
+    # )
+
+    # check cluster state
+    check_job_flow_task = EmrJobFlowSensor(
+        task_id="check_job_flow_task", 
+        job_flow_id=create_emr_cluster_task.output,
+        target_states=['TERMINATING'],
+        failed_states=['TERMINATED_WITH_ERRORS'],
+    )
+
+
+    upload_script_task >> create_emr_cluster_task >> check_job_flow_task
+
+
