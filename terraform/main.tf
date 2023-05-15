eb516658869381d5a6de4c716ca56635098a3f02
Kishan Patel <mail@kishanpatel.dev>
2023-05-15 19:48:08 -0400

    Start redshift in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,3 +70,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_data_lake" {
 
 
 # redshift instance for data warehouse
+resource "aws_redshift_cluster" "redshift_warehouse" {
+  cluster_identifier = "redshift-kpde"
+  database_name      = "dev"
+  master_username    = "service_wp"
+  master_password    = "serviceWp1"
+  node_type          = "dc2.large"
+  cluster_type       = "single-node"
+  publicly_accessible = true
+}
